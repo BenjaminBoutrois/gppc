c2b19de45940cd322c506b390796acdcb2c45c9c
BenjaminBoutrois <benjamin.boutrois28@gmail.com>
2019-04-16 17:17:25 +0200

    Ajout CSS

---
+++ forms/choices.py
@@ -0,0 +1,12 @@
+BUILDING_CHOICES = (
+                     (1, "Building 1"),
+                     (2, "Building 2"),
+                     (3, "Building 3"),
+                     (4, "Building 4")
+                 )
+
+FORMAT_CHOICES = (
+                      (1, "A0 (118.9 x 84.1cm)"),
+                      (2, "A0+ oversize (129 x 91cm)"),
+                      (3, "Other format")
+                  )
--- forms/form.py
+++ forms/form.py
@@ -1,10 +1,15 @@
 from django import forms
+from .choices import *
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    firstname = forms.CharField(max_length=100)
-    email = forms.CharField(max_length=100)
-    phone = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
-    envoyeur = forms.EmailField(label="Votre adresse e-mail")
-    renvoi = forms.BooleanField(help_text="Cochez si vous souhaitez obtenir une copie du mail envoyé.", required=False)
+    name = forms.CharField(max_length = 100)
+    firstname = forms.CharField(max_length = 100)
+    email = forms.CharField(max_length = 100)
+    phone = forms.CharField(max_length = 100)
+    building = forms.ChoiceField(choices=BUILDING_CHOICES)
+    office = forms.CharField(max_length = 100)
+    upload = forms.FileField()
+    format = forms.ChoiceField(choices=FORMAT_CHOICES)
+    formatHeight = forms.CharField(max_length = 100)
+    formatWidth = forms.CharField(max_length = 100)
+    comments = forms.CharField(widget = forms.Textarea)
