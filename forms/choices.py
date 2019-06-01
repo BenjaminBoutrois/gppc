8d832e3c307b5574f4eb166dad44c3ce91004f81
BenjaminBoutrois <benjamin.boutrois28@gmail.com>
2019-06-01 18:54:02 +0200

    Mise en page + ajout js format d'impression

--- forms/choices.py
+++ forms/choices.py
@@ -1,12 +1,10 @@
 BUILDING_CHOICES = (
-                     (1, "Building 1"),
-                     (2, "Building 2"),
-                     (3, "Building 3"),
-                     (4, "Building 4")
+                     (1, "GeePs"),
+                     (2, "Bréguet")
                  )
 
 FORMAT_CHOICES = (
                       (1, "A0 (118.9 x 84.1cm)"),
                       (2, "A0+ oversize (129 x 91cm)"),
-                      (3, "Other format")
+                      (3, "Autre format")
                   )
